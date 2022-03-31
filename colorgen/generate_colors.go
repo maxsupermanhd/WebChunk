3bdeca266b808a0c7980b789b5c5ba6d120fd884
maxsupermanhd <q3.max.2011@ya.ru>
2022-03-31 20:41:08 +0300

    First steps of implementing colors for blockstates

--- colorgen/generate_colors.go
+++ colorgen/generate_colors.go
@@ -1,152 +1,365 @@
 package main
 
 import (
+	"archive/zip"
 	"encoding/gob"
+	"encoding/json"
 	"fmt"
-	"image"
 	"image/color"
 	"image/png"
-	"io/fs"
-	"math"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
-	"path/filepath"
-	"runtime"
+	"reflect"
+	"regexp"
+	"sort"
 	"strings"
-	"sync"
 
-	"github.com/Tnze/go-mc/data/block"
+	"github.com/Tnze/go-mc/level/block"
+	"github.com/davecgh/go-spew/spew"
 )
 
-type trieNode struct {
-	next   map[rune]*trieNode
-	values []int
+var (
+	JARpath = `/home/max/.minecraft/versions/1.18.2/1.18.2.jar`
+)
+
+// type blockDescription struct {
+// 	state string
+// 	id    string
+// }
+
+func must(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
 }
 
-var de []fs.DirEntry
-var texturesDir = `/home/max/Desktop/block/`
+// func genBlockDescription(id string) []blockDescription {
+// 	if block.FromID[id] == nil {
+// 		return []blockDescription{}
+// 	}
+// 	params := map[string][]string{}
+// 	bp := reflect.New(reflect.ValueOf(block.FromID[id]).Type())
+// 	b := bp.Elem()
+// 	for m := 0; m < b.NumField(); m++ {
+// 		fieldName := b.Type().Field(m).Name
+// 	}
+// }
 
-func main() {
-	trie := &trieNode{}
-	for _, b := range block.ByID {
-		trie := trie
-		for _, c := range b.Name {
-			if trie.next == nil {
-				trie.next = make(map[rune]*trieNode)
-			}
-			if trie.next[c] == nil {
-				trie.next[c] = new(trieNode)
-			}
-			trie = trie.next[c]
+// func getBlockFromDescription(name, description string) block.Block {
+// 	if block.FromID[name] == nil {
+// 		return nil
+// 	}
+// 	if description == "" {
+// 		return block.FromID[name]
+// 	}
+// 	params := map[string]string{}
+// 	for _, d := range strings.Split(description, ",") {
+// 		kv := strings.Split(d, "=")
+// 		if len(kv) != 2 {
+// 			panic("kv pair is not a pair")
+// 		}
+// 		params[kv[0]] = kv[1]
+// 	}
+// 	bp := reflect.New(reflect.ValueOf(block.FromID[name]).Type())
+// 	b := bp.Elem()
+// 	for m := 0; m < b.NumField(); m++ {
+// 		fieldName := b.Type().Field(m).Name
+// 		methodUnmarshal := -1
+// 		for methodI := 0; methodI < b.Field(m).Addr().NumMethod(); methodI++ {
+// 			if b.Field(m).Addr().Type().Method(methodI).Name == "UnmarshalText" {
+// 				methodUnmarshal = methodI
+// 				break
+// 			}
+// 		}
+// 		if methodUnmarshal == -1 {
+// 			log.Printf("Failed to find method UnmarshalText in type [%s]", b.Field(m).Type().Name())
+// 			continue
+// 		}
+// 		for k, v := range params {
+// 			if k == strings.ToLower(fieldName) {
+// 				b.Field(m).Addr().Method(methodUnmarshal).Call([]reflect.Value{reflect.ValueOf([]byte(v))})
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return b.Interface().(block.Block)
+// }
+
+func blockMatches(b *block.Block, id string, d string) bool {
+	if d == "" {
+		return true
+	}
+	if b == nil {
+		panic(fmt.Errorf("block-description matcher called with nil pointer to a block"))
+	}
+	if (*b).ID() != id {
+		return false
+	}
+	params := map[string]string{}
+	for _, e := range strings.Split(d, ",") {
+		kv := strings.Split(e, "=")
+		if len(kv) != 2 {
+			panic("block-description matcher called with invalid description")
 		}
-		trie.values = make([]int, 0, 4)
+		params[kv[0]] = kv[1]
 	}
-
-	var err error
-	if de, err = os.ReadDir(texturesDir); err != nil {
-		panic(err)
-	}
-	for i, d := range de {
-		trie := trie
-		name := d.Name()
-		for _, c := range name {
-			if trie.next == nil {
-				trie.next = make(map[rune]*trieNode)
-			}
-			if trie.next[c] == nil {
-				trie.next[c] = new(trieNode)
-			}
-			trie = trie.next[c]
-
-			if trie.values != nil {
-				trie.values = append(trie.values, i)
+	r := reflect.ValueOf(*b)
+	for m := 0; m < r.Type().NumField(); m++ {
+		fieldName := r.Type().Field(m).Name
+		methodString := -1
+		for methodI := 0; methodI < r.Field(m).NumMethod(); methodI++ {
+			if r.Field(m).Type().Method(methodI).Name == "String" {
+				methodString = methodI
 				break
 			}
 		}
-	}
-
-	var wg sync.WaitGroup
-	tasks := make(chan *block.Block)
-	results := make([]*color.RGBA64, len(block.ByID))
-
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go func() {
-			for b := range tasks {
-				results[b.ID] = findColor(trie, b)
-				wg.Done()
+		if methodString == -1 {
+			if r.Field(m).Type().Name() == "Boolean" {
+				methodString = -2
+			} else if r.Field(m).Type().Name() == "Integer" {
+				methodString = -3
+			} else {
+				log.Printf("Failed to find method String in type [%s]", r.Field(m).Type().Name())
+				continue
 			}
-		}()
-	}
+		}
+		for k, v := range params {
+			if k == strings.ToLower(fieldName) {
+				if methodString == -2 {
+					if fmt.Sprint(r.Field(m).Bool()) != v {
+						return false
+					}
+				} else if methodString == -3 {
+					if fmt.Sprint(r.Field(m).Int()) != v {
+						return false
+					}
 
-	wg.Add(len(block.ByID))
-	for _, b := range block.ByID {
-		tasks <- b
-	}
-	close(tasks)
-	wg.Wait()
-	for i, v := range results {
-		if v == nil {
-			results[i] = &color.RGBA64{R: 0, G: 0, B: 0, A: 0}
+				} else {
+					e := r.Field(m).Method(methodString).Call([]reflect.Value{})
+					if len(e) != 1 {
+						panic(fmt.Errorf("block-description matcher got wrong return from String method"))
+					}
+					if e[0].String() != v {
+						return false
+					}
+					break
+				}
+			}
 		}
 	}
-	results[8] = &color.RGBA64{
-		R: 97 * math.MaxUint16 / 256,
-		G: 157 * math.MaxUint16 / 256,
-		B: 54 * math.MaxUint16 / 256,
-		A: 255 * math.MaxUint16 / 256,
-	}
-	fmt.Println(results)
-
-	size := int(math.Ceil(math.Sqrt(float64(len(results)))))
-	fmt.Println("Size: ", size)
-	img := image.NewRGBA(image.Rect(0, 0, size, size))
-	for i, v := range results {
-		if v != nil {
-			img.Set(i%size, i/size, v)
-		}
-	}
-	f, err := os.Create("res.png")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	if err := png.Encode(f, img); err != nil {
-		panic(err)
-	}
-
-	output(results)
+	return true
 }
 
-func find(root *trieNode, name string) []int {
-	if len(root.values) > 0 {
-		return root.values
-	}
-	if len(name) == 0 {
-		return nil
-	}
-	return find(root.next[[]rune(name)[0]], name[1:])
-}
-
-func findColor(t *trieNode, b *block.Block) *color.RGBA64 {
-	node := find(t, b.Name)
-	if len(node) <= 0 {
-		return nil
-	}
-	pic := de[node[0]]
-	for i := range node {
-		f := de[node[i]]
-		if strings.Contains(f.Name(), "top") && filepath.Ext(f.Name()) != ".mcdata" {
-			pic = de[node[i]]
-			break
+func getModel(vv interface{}) (map[string]interface{}, error) {
+	statedesc, ok := vv.(map[string]interface{})
+	if !ok {
+		statedesca, ok := vv.([]interface{})
+		if !ok {
+			return map[string]interface{}{}, fmt.Errorf("bad state description: %##v", vv)
+		}
+		if len(statedesc) < 0 {
+			return map[string]interface{}{}, fmt.Errorf("bad state description: %##v", vv)
+		}
+		statedesc, ok = statedesca[0].(map[string]interface{})
+		if !ok {
+			return map[string]interface{}{}, fmt.Errorf("bad state description: %##v", vv)
 		}
 	}
-	f, err := os.Open(filepath.Join(texturesDir, pic.Name()))
-	if err != nil {
-		panic(err)
+	return statedesc, nil
+	// model, ok := statedesc["model"]
+	// if !ok {
+	// 	return map[string]interface{}{}, fmt.Errorf("bad state description: %##v", vv)
+	// }
+	// modelpath, ok := model.(string)
+	// if !ok {
+	// 	return map[string]interface{}{}, fmt.Errorf("bad state description: %##v", vv)
+	// }
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	spew.Config.Indent = "   "
+
+	filepaths := map[string]*zip.File{}
+	// colors := map[string]color.RGBA64{}
+
+	blockstateRegex := regexp.MustCompile("assets/minecraft/blockstates/([A-Za-z_]+).json")
+
+	log.Printf("Opening jar [%s]", JARpath)
+	r, err := zip.OpenReader(JARpath)
+	must(err)
+	defer r.Close()
+	log.Print("Enumerating files in jar...")
+	for i := 0; i < len(r.File); i++ {
+		filepaths[r.File[i].Name] = r.File[i]
 	}
-	defer f.Close()
-	img, _, err := image.Decode(f)
+	log.Printf("Mapped %d filenames", len(r.File))
+
+	keys := make([]string, 0, len(filepaths))
+	for k := range filepaths {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return strings.Compare(keys[i], keys[j]) > 0
+	})
+
+	// for i, _ := range block.FromID {
+	// 	prefix := "assets/minecraft/textures/block/"
+	// 	blockname := strings.ReplaceAll(i, "minecraft:", "")
+	// 	var f *zip.File
+	// 	for j, _ := range filepaths {
+	// 		textureRegex := regexp.MustCompile("assets/minecraft/textures/block/([A-Za-z_]+).png")
+
+	// 	}
+	// 	// f, ok := filepaths[prefix+blockname+"_top.png"]
+	// 	// if !ok {
+	// 	// 	f, ok = filepaths[prefix+blockname+".png"]
+	// 	// 	if !ok {
+	// 	// 		log.Print("Failed to find texture for [" + blockname + "]")
+	// 	// 		colors[i] = color.RGBA64{0, 0, 0, 0}
+	// 	// 		continue
+	// 	// 	}
+	// 	// }
+	// 	d, err := f.Open()
+	// 	must(err)
+	// 	defer d.Close()
+	// 	colors[i] = *findColor(d)
+	// }
+
+	// log.Printf("Loaded %d/%d", len(colors), len(block.FromID))
+
+	log.Print("Mapping blockstates to models...")
+	statemodel := map[block.Block]map[string]interface{}{}
+	failed := []string{}
+	failedcount := 0
+	for fnid := range keys {
+		fname := keys[fnid]
+		f := filepaths[fname]
+		fmatch := blockstateRegex.FindStringSubmatch(fname)
+		if fmatch == nil {
+			continue
+		}
+		if fmatch[1] == "item_frame" || fmatch[1] == "glow_item_frame" {
+			continue
+		}
+		fr, err := f.Open()
+		must(err)
+		c, err := ioutil.ReadAll(fr)
+		must(err)
+		fr.Close()
+		v := map[string]interface{}{}
+		must(json.Unmarshal(c, &v))
+		_, ok := v["variants"]
+		if !ok {
+			spew.Dump(v)
+			// log.Print(spew.Sprint(v))
+			failed = append(failed, fmatch[1])
+			for stateid := range block.StateList {
+				if block.StateList[stateid].ID() == "minecraft:"+fmatch[1] {
+					failedcount++
+				}
+			}
+			continue
+		}
+		for k, vv := range v["variants"].(map[string]interface{}) {
+			// log.Printf("%s: %##v", fmatch[1], k)
+			matchingBlocks := []block.Block{}
+			for stateid := range block.StateList {
+				if block.StateList[stateid].ID() != "minecraft:"+fmatch[1] {
+					continue
+				}
+				if blockMatches(&block.StateList[stateid], "minecraft:"+fmatch[1], k) {
+					matchingBlocks = append(matchingBlocks, block.StateList[stateid])
+				}
+			}
+			if len(matchingBlocks) != 0 {
+				modelpath, err := getModel(vv)
+				must(err)
+				for mbi := range matchingBlocks {
+					statemodel[matchingBlocks[mbi]] = modelpath
+				}
+			} else {
+				log.Printf("Failed to create blockstate [%s] for block [%s]", k, "minecraft:"+fmatch[1])
+				return
+			}
+			// log.Printf("%##v: %##v", len(matchingBlocks), vv)
+		}
+	}
+	spew.Dump(failed)
+	log.Printf("States parsed/actually %v/%v (%v)", len(statemodel), len(block.StateList), failedcount)
+
+	// for i, j := range block.StateList {
+
+	// }
+
+	// var wg sync.WaitGroup
+	// tasks := make(chan *block.Block)
+	// results := map[string]color.RGBA64{}
+
+	// for i := 0; i < runtime.NumCPU(); i++ {
+	// 	go func() {
+	// 		for b := range tasks {
+	// 			results[b.ID] = findColor(trie, b)
+	// 			wg.Done()
+	// 		}
+	// 	}()
+	// }
+
+	// wg.Add(len(block.ByID))
+	// for _, b := range block.ByID {
+	// 	tasks <- b
+	// }
+	// close(tasks)
+	// wg.Wait()
+	// for i, v := range results {
+	// 	if v == nil {
+	// 		results[i] = &color.RGBA64{R: 0, G: 0, B: 0, A: 0}
+	// 	}
+	// }
+	// results[8] = &color.RGBA64{
+	// 	R: 97 * math.MaxUint16 / 256,
+	// 	G: 157 * math.MaxUint16 / 256,
+	// 	B: 54 * math.MaxUint16 / 256,
+	// 	A: 255 * math.MaxUint16 / 256,
+	// }
+	// fmt.Println(results)
+
+	// size := int(math.Ceil(math.Sqrt(float64(len(results)))))
+	// fmt.Println("Size: ", size)
+	// img := image.NewRGBA(image.Rect(0, 0, size, size))
+	// for i, v := range results {
+	// 	if v != nil {
+	// 		img.Set(i%size, i/size, v)
+	// 	}
+	// }
+	// f, err := os.Create("res.png")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+	// if err := png.Encode(f, img); err != nil {
+	// 	panic(err)
+	// }
+
+	// output(results)
+}
+
+// func find(root *trieNode, name string) []int {
+// 	if len(root.values) > 0 {
+// 		return root.values
+// 	}
+// 	if len(name) == 0 {
+// 		return nil
+// 	}
+// 	return find(root.next[[]rune(name)[0]], name[1:])
+// }
+
+func findColor(f io.ReadCloser) *color.RGBA64 {
+	img, err := png.Decode(f)
 	if err != nil {
-		panic(fmt.Errorf("decode %s error: %w", pic.Name(), err))
+		panic(fmt.Errorf("decode error: %w", err))
 	}
 	bounds := img.Bounds()
 	var rr, gg, bb, aa, count float64
@@ -169,7 +382,7 @@ func findColor(t *trieNode, b *block.Block) *color.RGBA64 {
 	}
 }
 
-func output(colors []*color.RGBA64) {
+func output(colors map[string]color.RGBA64) {
 	f, err := os.Create("colors.gob")
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -4,22 +4,18 @@ go 1.16
 
 require (
 	github.com/StackExchange/wmi v1.2.1 // indirect
-	github.com/Tnze/go-mc v1.17.2-0.20220220112341-23bcf9149a0e
+	github.com/Tnze/go-mc v1.18.2-0.20220319171725-4c2510565d56
 	github.com/davecgh/go-spew v1.1.1
 	github.com/dustin/go-humanize v1.0.0
 	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.1
-	github.com/georgysavva/scany v0.3.0 // indirect
-	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
-	github.com/go-gl/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec
+	github.com/georgysavva/scany v0.3.0
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
-	github.com/jackc/pgx/v4 v4.14.1
+	github.com/jackc/pgx/v4 v4.15.0
 	github.com/joho/godotenv v1.4.0
-	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/shirou/gopsutil v3.21.10+incompatible
@@ -28,5 +24,4 @@ require (
 	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
