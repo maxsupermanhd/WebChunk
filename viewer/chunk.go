3d18abc1d577b2d10a2b804ec62cdfd9316485bf
maxsupermanhd <q3.max.2011@ya.ru>
2022-05-03 16:58:15 +0300

    Improve save->level conversion

--- viewer/chunk.go
+++ viewer/chunk.go
@@ -24,8 +24,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/bits"
+	"strings"
 	"sync"
 	"time"
+	"unsafe"
 
 	"github.com/Tnze/go-mc/chat"
 	"github.com/Tnze/go-mc/data/packetid"
@@ -33,7 +36,9 @@ import (
 	"github.com/Tnze/go-mc/level/block"
 	"github.com/Tnze/go-mc/nbt"
 	pk "github.com/Tnze/go-mc/net/packet"
+	"github.com/Tnze/go-mc/save"
 	"github.com/Tnze/go-mc/server"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/google/uuid"
 	"github.com/maxsupermanhd/WebChunk/chunkStorage"
 )
@@ -130,7 +135,7 @@ func (s *chunkLoader) sendChunk(pos level.ChunkPos, p *playerData) {
 			log.Println("Chunk not found")
 			chunk = level.EmptyChunk(256)
 		} else {
-			chunk = level.ChunkFromSave(save, 256)
+			chunk = ActualChunkFromSave(save, 256)
 		}
 	}
 	if chunk == nil {
@@ -282,43 +287,181 @@ func (s *chunkLoader) SetPlayerWorldDim(u uuid.UUID, world, dim string) {
 	p.viewingChunks = map[level.ChunkPos]bool{}
 }
 
-func GetDefaultBlockEntity(n string, x, y, z int) *nbt.RawMessage {
-	switch {
-	case n == "banner":
-	case n == "barrel":
-	case n == "beacon":
-	case n == "bed":
-	case n == "beehive":
-	case n == "bell":
-	case n == "blast_furnace":
-	case n == "brewing_stand":
-	case n == "campfire":
-	case n == "chest":
-	case n == "comparator":
-	case n == "command_block":
-	case n == "conduit":
-	case n == "daylight_detector":
-	case n == "dispenser":
-	case n == "dropper":
-	case n == "enchanting_table":
-	case n == "ender_chest":
-	case n == "end_gateway":
-	case n == "end_portal":
-	case n == "furnace":
-	case n == "hopper":
-	case n == "jigsaw":
-	case n == "jukebox":
-	case n == "lectern":
-	case n == "mob_spawner":
-	case n == "piston":
-	case n == "shulker_box":
-	case n == "sign":
-	case n == "skull":
-	case n == "smoker":
-	case n == "soul_campfire":
-	case n == "structure_block":
-	case n == "trapped_chest":
-	default:
+func ActualChunkFromSave(c *save.Chunk, secs int) *level.Chunk {
+	sections := make([]level.Section, secs)
+	for _, v := range c.Sections {
+		var blockCount int16
+		stateData := *(*[]uint64)((unsafe.Pointer)(&v.BlockStates.Data))
+		statePalette := v.BlockStates.Palette
+		stateRawPalette := make([]int, len(statePalette))
+		for i, v := range statePalette {
+			b, ok := block.FromID[v.Name]
+			if !ok {
+				return nil
+			}
+			if v.Properties.Data != nil {
+				err := v.Properties.Unmarshal(&b)
+				if err != nil {
+					return nil
+				}
+			}
+			s := block.ToStateID[b]
+			if !isAir(s) {
+				blockCount++
+			}
+			stateRawPalette[i] = s
+		}
+
+		biomesData := *(*[]uint64)((unsafe.Pointer)(&v.Biomes.Data))
+		biomesPalette := v.Biomes.Palette
+		biomesRawPalette := make([]int, len(biomesPalette))
+		for i, v := range biomesPalette {
+			biomesRawPalette[i] = biomesIDs[strings.TrimPrefix(v, "minecraft:")]
+		}
+
+		i := int32(v.Y) - c.YPos
+		sections[i].BlockCount = blockCount
+		sections[i].States = level.NewStatesPaletteContainerWithData(16*16*16, stateData, stateRawPalette)
+		sections[i].Biomes = level.NewBiomesPaletteContainerWithData(4*4*4, biomesData, biomesRawPalette)
 	}
-	return &nbt.RawMessage{}
+	for i := range sections {
+		if sections[i].States == nil {
+			sections[i] = level.Section{
+				BlockCount: 0,
+				States:     level.NewStatesPaletteContainer(16*16*16, 0),
+				Biomes:     level.NewBiomesPaletteContainer(4*4*4, 0),
+			}
+		}
+	}
+	motionBlocking := *(*[]uint64)(unsafe.Pointer(&c.Heightmaps.MotionBlocking))
+	worldSurface := *(*[]uint64)(unsafe.Pointer(&c.Heightmaps.WorldSurface))
+	ret := level.Chunk{
+		Sections: sections,
+		HeightMaps: level.HeightMaps{
+			MotionBlocking: level.NewBitStorage(bits.Len(uint(secs)), 16*16, motionBlocking),
+			WorldSurface:   level.NewBitStorage(bits.Len(uint(secs)), 16*16, worldSurface),
+		},
+	}
+	var blockEntitiesData []nbt.RawMessage
+	err := c.BlockEntities.Unmarshal(&blockEntitiesData)
+	if err != nil {
+		log.Println("Error unmarshling block entities: " + err.Error())
+		return &ret
+	}
+	for _, rawdata := range blockEntitiesData {
+		var be map[string]interface{}
+		err = rawdata.Unmarshal(&be)
+		if err != nil {
+			log.Printf("Failed to unmarshal raw block entity data: %s", err.Error())
+			continue
+		}
+		var x, y, z int32
+		var ok bool
+		var id string
+		if x, ok = be["x"].(int32); !ok {
+			log.Printf("Invalid block entity, x wrong: %##v", be)
+			spew.Dump(be)
+			continue
+		}
+		if y, ok = be["y"].(int32); !ok {
+			log.Printf("Invalid block entity, y wrong: %##v", be)
+			spew.Dump(be)
+			continue
+		}
+		if z, ok = be["z"].(int32); !ok {
+			log.Printf("Invalid block entity, z wrong: %##v", be)
+			spew.Dump(be)
+			continue
+		}
+		if id, ok = be["id"].(string); !ok {
+			log.Printf("Invalid block entity, id wrong: %##v", be)
+			spew.Dump(be)
+			continue
+		}
+		ret.BlockEntity = append(ret.BlockEntity, level.BlockEntity{
+			XZ:   int8(((x & 15) << 4) | (z & 15)),
+			Y:    int16(y - 64),
+			Type: BlockEntityTypes[strings.TrimPrefix(id, "minecraft:")],
+			Data: rawdata,
+		})
+		log.Printf("Block entity stored: %s, %s", id, rawdata.String())
+	}
+	for iii := range ret.BlockEntity {
+		log.Printf("Block entity sent: %d, %s", ret.BlockEntity[iii].Type, ret.BlockEntity[iii].Data.String())
+	}
+	_ = ret.BlockEntity
+	return &ret
+}
+
+var biomesIDs = map[string]int{
+	"the_void":                 0,
+	"plains":                   1,
+	"sunflower_plains":         2,
+	"snowy_plains":             3,
+	"ice_spikes":               4,
+	"desert":                   5,
+	"swamp":                    6,
+	"forest":                   7,
+	"flower_forest":            8,
+	"birch_forest":             9,
+	"dark_forest":              10,
+	"old_growth_birch_forest":  11,
+	"old_growth_pine_taiga":    12,
+	"old_growth_spruce_taiga":  13,
+	"taiga":                    14,
+	"snowy_taiga":              15,
+	"savanna":                  16,
+	"savanna_plateau":          17,
+	"windswept_hills":          18,
+	"windswept_gravelly_hills": 19,
+	"windswept_forest":         20,
+	"windswept_savanna":        21,
+	"jungle":                   22,
+	"sparse_jungle":            23,
+	"bamboo_jungle":            24,
+	"badlands":                 25,
+	"eroded_badlands":          26,
+	"wooded_badlands":          27,
+	"meadow":                   28,
+	"grove":                    29,
+	"snowy_slopes":             30,
+	"frozen_peaks":             31,
+	"jagged_peaks":             32,
+	"stony_peaks":              33,
+	"river":                    34,
+	"frozen_river":             35,
+	"beach":                    36,
+	"snowy_beach":              37,
+	"stony_shore":              38,
+	"warm_ocean":               39,
+	"lukewarm_ocean":           40,
+	"deep_lukewarm_ocean":      41,
+	"ocean":                    42,
+	"deep_ocean":               43,
+	"cold_ocean":               44,
+	"deep_cold_ocean":          45,
+	"frozen_ocean":             46,
+	"deep_frozen_ocean":        47,
+	"mushroom_fields":          48,
+	"dripstone_caves":          49,
+	"lush_caves":               50,
+	"nether_wastes":            51,
+	"warped_forest":            52,
+	"crimson_forest":           53,
+	"soul_sand_valley":         54,
+	"basalt_deltas":            55,
+	"the_end":                  56,
+	"end_highlands":            57,
+	"end_midlands":             58,
+	"small_end_islands":        59,
+	"end_barrens":              60,
+}
+
+func isAir(s int) bool {
+	switch block.StateList[s].(type) {
+	case block.Air, block.CaveAir, block.VoidAir:
+		return true
+	default:
+		return false
+	}
 }
