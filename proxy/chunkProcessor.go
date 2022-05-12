e78a0566d5699baaf12cf0e5b93ee198224e9325
maxsupermanhd <q3.max.2011@ya.ru>
2022-05-12 03:38:29 +0300

    Make custom reader for chunk data packet

--- proxy/chunkProcessor.go
+++ proxy/chunkProcessor.go
@@ -21,7 +21,10 @@
 package proxy
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"strings"
 
@@ -69,8 +72,10 @@ func packetAcceptor(recv chan pk.Packet, conn *server.PacketQueue, resp chan *Pr
 	}
 	c := map[cachePos]cacheChunk{}
 	type loadedDim struct {
-		id   int32
-		minY int32
+		id          int32
+		minY        int32
+		height      int32
+		totalHeight int32
 	}
 	loadedDims := map[string]loadedDim{}
 	currentDim := ""
@@ -81,12 +86,9 @@ func packetAcceptor(recv chan pk.Packet, conn *server.PacketQueue, resp chan *Pr
 				log.Println("Recieved chunk without dimension")
 				continue
 			}
-			var cpos level.ChunkPos
-			// var cc level.Chunk
-			cc := *level.EmptyChunk(25)
-			err := p.Scan(&cpos, &cc)
+			cpos, cc, err := deserializeChunkPacket(p)
 			if err != nil {
-				log.Printf("Failed to scan chunk packet: %v", err.Error())
+				log.Printf("Failed to parse chunk data packet: %s", err.Error())
 				continue
 			}
 			// verify all block entities are present
@@ -359,12 +361,17 @@ func packetAcceptor(recv chan pk.Packet, conn *server.PacketQueue, resp chan *Pr
 					spew.Dump(de)
 					continue
 				}
-				loadedDims[dimname] = struct {
-					id   int32
-					minY int32
-				}{
-					id:   dimid,
-					minY: miny,
+				height, ok := de["height"].(int32)
+				if !ok {
+					log.Println("Dimension registry value miny error")
+					spew.Dump(de)
+					continue
+				}
+				loadedDims[dimname] = loadedDim{
+					id:          dimid,
+					minY:        miny,
+					height:      height,
+					totalHeight: height - miny,
 				}
 			}
 		}
@@ -375,4 +382,141 @@ func packetAcceptor(recv chan pk.Packet, conn *server.PacketQueue, resp chan *Pr
 			pk.UUID(uuid.Nil),
 		))
 	}
+}
+
+// and now we need to guess how long is sections array in this packet...
+
+// var cc level.Chunk
+// always EOF
+
+// cc := level.Chunk{
+// 	Sections: []level.Section{},
+// 	HeightMaps: level.HeightMaps{
+// 		MotionBlocking: &level.BitStorage{},
+// 		WorldSurface:   &level.BitStorage{},
+// 	},
+// 	BlockEntity: []level.BlockEntity{},
+// }
+// always EOF
+
+// cc := *level.EmptyChunk(16)
+// we must now length, wiki says
+// > The number of elements in the array is calculated based on the world's height
+// except server sends how many sections he wants instead of actual required section count
+
+// dim, ok := loadedDims[currentDim]
+// if !ok {
+// 	log.Printf("Recieved chunk data without dimension?!")
+// 	continue
+// }
+// log.Printf("%s: % 4d % 4d % 4d", currentDim, dim.minY, dim.height, dim.totalHeight)
+// cc := *level.EmptyChunk(int(dim.totalHeight) / 16)
+// seems to be correct to do such calculation but oh well, it does not work this way...
+
+// cclen := 64 // MAGIC: theoretical maximum of world height
+// cc := *level.EmptyChunk(cclen)
+// err := p.Scan(&cpos, &cc)
+// for errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+// 	cclen--
+// 	cc = *level.EmptyChunk(cclen)
+// 	err = p.Scan(&cpos, &cc)
+// }
+// this might seem to be a complete rofl and not be like this but I don't have choice
+// completely tanks performance with allocations, not a solution...
+// log.Printf("Scanned with len of %d", cclen)
+
+func deserializeChunkPacket(p pk.Packet) (level.ChunkPos, level.Chunk, error) {
+	var (
+		heightmaps struct {
+			MotionBlocking []uint64 `nbt:"MOTION_BLOCKING"`
+			WorldSurface   []uint64 `nbt:"WORLD_SURFACE"`
+		}
+		sectionsData pk.ByteArray
+		cc           level.Chunk
+		cpos         level.ChunkPos
+	)
+	err := p.Scan(&cpos, &pk.Tuple{
+		pk.NBT(&heightmaps),
+		&sectionsData,
+		pk.Array(&cc.BlockEntity),
+		&lightData{
+			SkyLightMask:   make(pk.BitSet, (16*16*16-1)>>6+1),
+			BlockLightMask: make(pk.BitSet, (16*16*16-1)>>6+1),
+			SkyLight:       []pk.ByteArray{},
+			BlockLight:     []pk.ByteArray{},
+		},
+	})
+	if err != nil {
+		return cpos, cc, err
+	}
+	// cc.HeightMaps.MotionBlocking = heightmaps.MotionBlocking
+	d := bytes.NewReader(sectionsData)
+	dl := int64(len(sectionsData))
+	for {
+		if dl == 0 {
+			break
+		}
+		if dl < 200 { // whole chunk structure is 207 if completely empty?
+			log.Printf("Leaving %d bytes behind while parsing chunk data!", dl)
+			break
+		}
+		ss := &level.Section{
+			BlockCount: 0,
+			States:     level.NewStatesPaletteContainer(16*16*16, 0),
+			Biomes:     level.NewBiomesPaletteContainer(4*4*4, 0),
+		}
+		n, err := ss.ReadFrom(d)
+		if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+			log.Printf("EOF while decoding chunk data while reading section %d", len(cc.Sections))
+			break
+		}
+		if n == 0 {
+			log.Printf("Failed to read from buffer? 0 readed")
+			continue
+		}
+		dl -= n
+		cc.Sections = append(cc.Sections, *ss)
+	}
+	return cpos, cc, err
+}
+
+type lightData struct {
+	SkyLightMask   pk.BitSet
+	BlockLightMask pk.BitSet
+	SkyLight       []pk.ByteArray
+	BlockLight     []pk.ByteArray
+}
+
+func bitSetRev(set pk.BitSet) pk.BitSet {
+	rev := make(pk.BitSet, len(set))
+	for i := range rev {
+		rev[i] = ^set[i]
+	}
+	return rev
+}
+
+func (l *lightData) WriteTo(w io.Writer) (int64, error) {
+	return pk.Tuple{
+		pk.Boolean(true), // Trust Edges
+		l.SkyLightMask,
+		l.BlockLightMask,
+		bitSetRev(l.SkyLightMask),
+		bitSetRev(l.BlockLightMask),
+		pk.Array(l.SkyLight),
+		pk.Array(l.BlockLight),
+	}.WriteTo(w)
+}
+
+func (l *lightData) ReadFrom(r io.Reader) (int64, error) {
+	var TrustEdges pk.Boolean
+	var RevSkyLightMask, RevBlockLightMask pk.BitSet
+	return pk.Tuple{
+		&TrustEdges, // Trust Edges
+		&l.SkyLightMask,
+		&l.BlockLightMask,
+		&RevSkyLightMask,
+		&RevBlockLightMask,
+		pk.Array(&l.SkyLight),
+		pk.Array(&l.BlockLight),
+	}.ReadFrom(r)
 }
