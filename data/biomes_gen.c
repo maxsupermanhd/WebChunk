1c5f51214ff6bfb63fc0181bfef7c6d1afb66d9b
maxsupermanhd <q3.max.2011@ya.ru>
2022-06-06 16:18:34 +0300

    Add biome IDs

--- data/biomes_gen.c
+++ data/biomes_gen.c
@@ -7,16 +7,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "cubiomes/util.h"
+#include "cubiomes/layers.h"
 
 int main() {
 	unsigned char b[256][3];
 	initBiomeColors(b);
 	printf("// this file is generated with biome_gen.c\n");
 	printf("package biomes\n\nimport \"image/color\"\n\n");
-	printf("var biomeColors = map[int]color.RGBA{\n");
+	printf("var BiomeColors = map[int]color.RGBA{\n");
 	for(int i = 0; i < 256; i++) {
 		printf("\t%d: {%d, %d, %d, 255},\n", i, b[i][0], b[i][1], b[i][2]);
 	}
-	printf("}\n");
+	printf("}\n\n");
+	printf("var BiomeID = map[string]int{\n");
+	for(int i = 0; i < 256; i++) {
+		const char* s = biome2str(MC_1_19, i);
+		if(s != NULL) {
+			printf("\t\"%s\": %d,\n", s, i);
+		}
+	}
+	printf("}\n\n");
 	return 0;
 }
